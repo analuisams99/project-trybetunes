9aa2bc3bbe7267c8446952314e0def167c19cff0
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-02 20:44:46 -0300

    Cria links de navegaçao no cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -37,6 +38,11 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         {(loading) ? <Loading /> : this.renderUserName()}
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </nav>
       </header>
     );
   }
