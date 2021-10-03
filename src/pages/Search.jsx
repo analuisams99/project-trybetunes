f8bff266f17dcfd700b2383cdb9386b9e9b71dad
Ana Luisa <ana.luisa.isa@hotmail.com>
2021-10-02 21:19:57 -0300

    Cria formulario para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      artistName: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({
+      artistName: value,
+    });
+  }
+
   render() {
+    const { artistName } = this.state;
+    const MIN_CARACTHERS = 2;
     return (
       <div data-testid="page-search">
         <Header />
+        <form action="submit">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+            value={ artistName }
+            placeholder="Digite aqui..."
+          />
+          <label htmlFor="search-btn">
+            <input
+              type="button"
+              data-testid="search-artist-button"
+              value="Pesquisar"
+              id="search-btn"
+              disabled={ artistName.length < MIN_CARACTHERS }
+            />
+          </label>
+        </form>
       </div>
     );
   }
